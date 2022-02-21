30d9aefd0850e917e5a32e88c1c80e0f57ea68ea
jiayuehan <99764953+jiayuehan@users.noreply.github.com>
2022-02-21 18:41:37 -0500

    Update tweet_analysis.py

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -73,10 +73,58 @@ class tweet_nlp:
                 plt.xlabel('Polarity Score')
 
                 plt.tight_layout(w_pad=6)
+              
+    def nlp_seaborn(self, dic, list_files):
+        '''graph the average sentence length, polarity, subjectivity, and readability score of all three articles'''
+        
+        # the values that the graph does not need and need
+        del_keys = ['total_word_count', 'sentence_count', 'avg_word_length', 'word_counts', 'text']
+        nlp_columns = ['avg_sentence_length','polarity','subjectivity','readability']
+        
+        # initialize the dataframe
+        df = pd.DataFrame(columns = nlp_columns)
+        
+        # delete the values that we don't need in the dic
+        for n in list_files:
+            new_dict = dic[n]
+            for key in del_keys:
+                del new_dict[key]
+            
+       # generate a tweet sentiment analysis using the dictionaty
+        for n in list_files:
+            df = df.append(pd.DataFrame(dic[n], columns = nlp_columns, index = [n]))
+        
+        # graph the figure
+        fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10, 10), sharex=True)
+        x = ['Audi','BMW','Mercedes']
+        y1 = df.avg_sentence_length
+        sns.barplot(x=x,y=y1,ax=ax1, palette='RdPu')
+        ax1.set_ylabel('Average Sentence Length')
+        ax1.set_title('Comparison of Text Structural Measures')
+        ax1.set_xlabel('')
+        
+        y2 = df.readability
+        sns.barplot(x=x,y=y2,ax=ax2, palette='BuPu')
+        ax2.set_ylabel('Readability')
+        ax2.set_xlabel('')
+        
+        y3 = df.subjectivity
+        sns.barplot(x=x,y=y3,ax=ax3, palette='BuPu') 
+        ax3.set_ylabel('Subjectivity') 
+        ax3.set_xlabel('')
+        
+        y4 = df.polarity 
+        sns.barplot(x=x,y=y4,ax=ax4, palette='BuPu')
+        ax4.set_ylabel('Polarity')
+        ax4.set_xlabel('')
+        
+        sns.despine(bottom=False)
+        plt.tight_layout(h_pad=1)
+        
        
 
 def main():
-
-    car_data = tweet_nlp(['car_tweets_AUDI.csv',
-                     'car_tweets_BMW.csv','car_tweets_MERCEDES.csv'])
+    files = ['car_tweets_AUDI.csv','car_tweets_BMW.csv', 'car_tweets_MERCEDES.csv']
+    car_data = tweet_nlp(files)
     car_data.polar_sent(['car_tweets_AUDI.csv', 'car_tweets_BMW.csv'],['Audi','BWM','Benz'])
+    car_data.nlp_seaborn(tweet_dict, files)
