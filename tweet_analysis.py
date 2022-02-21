7d3994745a99d439b404b6f2bd97af4d6cdeb246
Yang Bao <93554411+yangbao328@users.noreply.github.com>
2022-02-21 09:54:44 -0500

    Update tweet_analysis.py

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -1,4 +1,7 @@
-class texttt:
+import requests
+import numpy as np
+
+class tweet_nlp:
     from collections import Counter, defaultdict
 
     def __init__(self):
@@ -29,15 +32,13 @@ class texttt:
         
         
     def read_text(filename, parser=None):
-    #for name in filename:
+    
         columns = ['brand','date','ID','content','retweet_ct','fav_ct']
         file = pd.read_csv(filename, sep=',',  names=columns)
     return file
-    import requests
+    
     def load_stop_words(stopfile=None):
-
-        #stopf = input('Would you import a stopwords file? Sumbit the file name here pls:')
-
+    
         if stopfile is None:
             stopwords_list = requests.get("https://gist.githubusercontent.com/rg089/35e00abf8941d72d419224cfd5b5925d/raw/12d899b70156fd0041fa9778d657330b024b959c/stopwords.txt").content
             stopwords = stopwords_list.decode().splitlines()
@@ -47,5 +48,23 @@ class texttt:
                 for line in f:
                     stopf = [line.split(',') for line in f]
             text = ' '.join([word for word in text_con.split() if word not in stopf])
-    # A list of common or stop words.  These get filtered from each file automatically 
+        # A list of common or stop words.  These get filtered from each file automatically 
         return text
+
+    def polarity_by_sent(files):
+        
+        # create the first of two panels and set current axis
+        plt.subplot(3, 1, 1) # (rows, columns, panel number)
+        plt.title('Audi Polarity by Sentence', fontsize=13)
+        plt.bar(sent_word_audi, polar_audi, label='Audi')
+
+        # create the second panel and set current axis
+        plt.subplot(3, 1, 2)
+        plt.title('BMW Polarity by Sentence', fontsize=13)
+        plt.bar(sent_word_bmw, polar_bmw, label='BMW');
+
+        plt.subplot(3, 1, 3)
+        plt.title('Benz Polarity by Sentence', fontsize=13)
+        plt.bar(sent_word_benz, polar_benz, label='Benz');
+        plt.figlegend(loc='upper right', ncol=1, labelspacing=0.3, fontsize=8, bbox_to_anchor=(1.11, 0.9))
+        plt.tight_layout(w_pad=6)
